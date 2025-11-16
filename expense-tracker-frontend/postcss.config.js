ad492a93171a3501d24a9cadeb7016fdb25732fe
Gaurav Dhayade <127874882+Gauravdhayade@users.noreply.github.com>
2025-11-16 17:17:52 +0530

    Delete expense-tracker-frontend directory

--- expense-tracker-frontend/postcss.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {}, // ✅ Tailwind v4 compatible plugin
-  },
-};
--- expense-tracker-frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/expense-tracker/',   // 👈 हा तुझ्या GitHub repo नावाशी जुळायला हवा
-})
